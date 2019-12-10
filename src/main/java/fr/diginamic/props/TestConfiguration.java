2f433e7bc45016fa6ffea59b2d789e1534442b3a
Roux JP <jproux343@gmail.com>
2019-12-10 10:06:44 +0100

    TP 01 : fichier de properties

---
+++ src/main/java/fr/diginamic/props/TestConfiguration.java
@@ -0,0 +1,19 @@
+package fr.diginamic.props;
+import java.util.ResourceBundle;
+
+public class TestConfiguration {
+
+	public static void main(String[] args) {
+		ResourceBundle monFicConf = ResourceBundle.getBundle( "FichConf");
+		String valParam1 = monFicConf.getString("clef1");
+		System.out.println( "valParam1 : " + valParam1);
+		String valParam = "";
+		for( int i = 1 ; i < 5 ; i++) {
+			valParam = monFicConf.getString("clef" + i);
+			System.out.println( "valParam" + i + " : " + valParam);
+			
+		}
+
+	}
+
+}
